0806540ea7d5c13186418bdf051640adefe7c010
ClaytonWheeler0205 <Clayton02.Wheeler05@gmail.com>
2023-07-31 22:47:19 -0500

    Added Actor List View
    Added a dynamic actor list view that shows all the actor SO in the assets folder. WIP implementing what happens when selecting an actor.

--- Assets/Editor/DialogueTreePlanter/Utilities/ActorGatherer/ActorGathererImpl.cs
+++ Assets/Editor/DialogueTreePlanter/Utilities/ActorGatherer/ActorGathererImpl.cs
@@ -1,5 +1,6 @@
 using DialogueTreePlanter.Actors;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEditor;
 
 namespace DialogueTreePlanter.Utilities
@@ -10,9 +11,10 @@ namespace DialogueTreePlanter.Utilities
         public override List<SO_Actor> GetActorsList()
         {
             List<SO_Actor> actors = new List<SO_Actor>();
+            GetActorAssets();
             foreach(string asset in actorAssetFiles)
             {
-                actors.Add(AssetDatabase.LoadAssetAtPath<SO_Actor>(actorsPath + asset));
+                actors.Add(AssetDatabase.LoadAssetAtPath<SO_Actor>(asset));
             }
             return actors;
         }
--- Assets/Editor/DialogueTreePlanter/Windows/CastingCallWindow.cs
+++ Assets/Editor/DialogueTreePlanter/Windows/CastingCallWindow.cs
@@ -12,7 +12,9 @@ namespace DialogueTreePlanter.Windows
     public class CastingCallWindow : EditorWindow
     {
         private VisualElement leftPane;
+        TwoPaneSplitView splitView;
         private VisualElement rightPane;
+        private ListView actorsListView;
 
         private static ActorFactory _actorFactory;
         private static ActorGatherer _actorGatherer;
@@ -24,13 +26,20 @@ namespace DialogueTreePlanter.Windows
         {
             EditorWindow window = GetWindow<CastingCallWindow>();
             window.titleContent = new GUIContent("Casting Call");
+        }
+
+        private void Awake()
+        {
             _actorFactory = new ConcreteActorFactory();
             _actorGatherer = new ActorGathererImpl();
+            _actors = _actorGatherer.GetActorsList();
         }
 
         public void CreateGUI()
         {
-            _actors = _actorGatherer.GetActorsList();
+            CreateDualPanels();
+            leftPane.Add(CreateNewActorButton());
+            CreateListView();
         }
 
         private Button CreateNewActorButton()
@@ -39,8 +48,45 @@ namespace DialogueTreePlanter.Windows
             newActorButton.clicked += () =>
             {
                 _actorFactory.CreateActorAsset();
+                _actors = _actorGatherer.GetActorsList();
+                actorsListView.itemsSource = _actors;
+                actorsListView.RefreshItems();
             };
             return newActorButton;
+        }
+
+        private void CreateDualPanels()
+        {
+            splitView = new TwoPaneSplitView(0, 250, TwoPaneSplitViewOrientation.Horizontal);
+            rootVisualElement.Add(splitView);
+
+            leftPane = new VisualElement();
+            splitView.Add(leftPane);
+
+            rightPane = new VisualElement();
+            splitView.Add(rightPane);
+        }
+
+        private void CreateListView()
+        {
+            actorsListView = new ListView();
+            leftPane.Add(actorsListView);
+
+            actorsListView.makeItem = () => new Label();
+            actorsListView.bindItem = (item, index) => { (item as Label).text = _actors[index].name; };
+            actorsListView.itemsSource = _actors;
+            actorsListView.onSelectionChange += OnActorSelectionChange;
+        }
+
+        private void OnActorSelectionChange(IEnumerable<object> actors)
+        {
+            rightPane.Clear();
+
+            SO_Actor actor = actors.First() as SO_Actor;
+            if (actor == null) { return; }
+
+            // WIP work on actor editor in right pane of window
+            Debug.Log(actor.name);
         }
     }
 }
